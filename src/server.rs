5f4334076a6af89059719e23d24ab6d62fd771de
Edward Tate <cmalune@gmail.com>
2018-11-20 12:06:56 +0100

    Refactored server functions into swim protocol.

--- src/server.rs
+++ src/server.rs
@@ -1,148 +1,52 @@
-use futures::sync::mpsc;
 use std::io;
 use std::net::SocketAddr;
 use std::thread;
+use std::sync::Arc;
+use futures::sync::mpsc;
 use tokio;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
 use uuid::Uuid;
 use bincode_channel::BincodeChannel;
 use cache::TimeoutCache;
-use client::{run_send, spawn_send};
 use membership::Membership;
 use dissemination::Dissemination;
 use message::{Message, Gossip};
+use swim::Swim;
 
 #[derive(Clone)]
 pub struct Server {
-    pub uuid: Uuid,
     pub addr: SocketAddr,
+    pub swim: Arc<Swim>,
 }
 
 impl Server {
-    pub fn new(addr: SocketAddr) -> Server {
+    pub fn new(addr: SocketAddr, swim: Swim) -> Server {
         Server {
-            addr,
-            uuid: Uuid::new_v4(),
+            addr: addr.clone(),
+            swim: Arc::new(swim),
         }
     }
 
-    pub fn send_bootstrap_join(&self, seed_addr: SocketAddr) {
-        let message = Message::Join(self.uuid.clone(), self.addr.clone());
-        run_send(seed_addr, message);
+    pub fn bootstrap(&self, bootstrap_addr: SocketAddr) {
+        self.swim.send_bootstrap_join(bootstrap_addr);
     }
 
-    fn send_join(&self, peer_addr: SocketAddr) {
-        let message = Message::Join(self.uuid.clone(), self.addr.clone());
-        spawn_send(peer_addr, message);
-    }
-
-    fn send_ack(&self, peer_addr: SocketAddr, gossip: Vec<Gossip>) {
-        let message = Message::Ack(self.uuid.clone(), gossip);
-        spawn_send(peer_addr, message);
-    }
-
-    fn send_ping(&self, peer_addr: SocketAddr) {
-        let message = Message::Ping(self.uuid.clone(), vec![]);
-        spawn_send(peer_addr, message);
-    }
-
-    fn process_gossip<'a>(&'a self, gossip: Gossip, membership: &'a Membership, dissemination: &'a Dissemination) {
-        match gossip.clone() {
-            Gossip::Join(peer_uuid, peer_addr_string) => {
-                if peer_uuid != self.uuid {
-                    println!("[server] GOSSIP: {:?}", gossip);
-                    let peer_addr = peer_addr_string.parse().unwrap();
-                    if membership.process_join(peer_uuid, peer_addr) {
-                        self.send_join(peer_addr);
-                        dissemination.gossip_join(peer_uuid, peer_addr);
-                    }
-                }
-            }
-            _ => ()
-        }
-    }
-
-    fn process_input(
-        self,
-        message: Message,
-        mut membership: Membership,
-        mut dissemination: Dissemination,
-        cache: TimeoutCache,
-    ) {
-        println!("[server] RECV: {:?}", message.clone());
+    fn process_input(self, message: Message) {
+        // println!("[server] RECV: {:?}", message.clone());
         match message.clone() {
-            Message::Join(peer_uuid, peer_addr) => {
-                if membership.process_join(peer_uuid, peer_addr) {
-                    self.send_join(peer_addr);
-                    dissemination.gossip_join(peer_uuid, peer_addr);
-                } else {
-                    println!("[server] Failed to process join request for {:?}", peer_addr);
-                }
-            }
-            Message::Ping(peer_uuid, gossip_vec) => {
-                match membership.get(peer_uuid) {
-                    Some(entry) => {
-                        // Process dissemination information
-                        for gossip in gossip_vec {
-                            self.process_gossip(gossip, &membership, &dissemination);
-                        }
-                        // Attach dissemination information
-                        let (peer_addr, _) = entry.value();
-                        let gossip = dissemination.acquire_gossip(membership.clone(), 1);
-                        self.send_ack(*peer_addr, gossip);
-                    }
-                    None => {
-                        println!("[server] unknown peer");
-                    }
-                }
-            }
-            Message::Ack(peer_uuid, gossip_vec) => {
-                match membership.get(peer_uuid) {
-                    Some(_) => {
-                        // Process dissemination information
-                        for gossip in gossip_vec {
-                            self.process_gossip(gossip, &membership, &dissemination);
-                        }
-                        // Remove timeout if exists
-                        println!("[server] Removing timeout for {:?}", peer_uuid.clone());
-                        cache.remove_ack_timeout(peer_uuid);
-                    }
-                    None => {
-                        println!("[server] unknown peer");
-                    }
-                }
-            }
-            Message::PingReq(peer_uuid, suspect_uuid) => {
-                match membership.get(peer_uuid) {
-                    Some(entry) => {
-                        let (peer_addr, _) = entry.value();
-                        match membership.get(suspect_uuid) {
-                            Some(entry) => {
-                                let (suspect_addr, _) = entry.value();
-                                // If this node is a suspect, immediately ack
-                                if self.addr == *suspect_addr {
-                                    let gossip = dissemination.acquire_gossip(membership.clone(), 1);
-                                    self.send_ack(*peer_addr, gossip);
-                                } else {
-                                    // Otherwise initiate a probe
-                                    self.send_ping(*suspect_addr);
-                                }
-                            }
-                            None => {
-                                println!("[server] unknown suspect");
-                            }
-                        }
-                    }
-                    None => {
-                        println!("[server] unknown peer");
-                    }
-                }
-            }
+            Message::Join(peer_uuid, peer_addr) =>
+                self.swim.handle_join(peer_uuid, peer_addr),
+            Message::Ping(peer_uuid, gossip_vec) =>
+                self.swim.handle_ping(peer_uuid, gossip_vec),
+            Message::Ack(peer_uuid, gossip_vec) =>
+                self.swim.handle_ack(peer_uuid, gossip_vec),
+            Message::PingReq(peer_uuid, suspect_uuid) =>
+                self.swim.handle_ping_req(peer_uuid, suspect_uuid),
         }
     }
 
-    fn handle_connection(self, socket: TcpStream, membership: Membership, dissemination: Dissemination, cache: TimeoutCache) {
+    fn handle_connection(self, socket: TcpStream) {
         // Splits the socket stream into bincode reader / writers
         let message_channel = BincodeChannel::<Message>::new(socket);
 
@@ -154,9 +58,7 @@ impl Server {
         let input_reader = message_channel
             .reader
             .for_each(move |message| {
-                let () = self
-                    .clone()
-                    .process_input(message, membership.clone(), dissemination.clone(), cache.clone());
+                let () = self.clone().process_input(message);
                 Ok(())
             }).map_err(|err| {
                 println!("[server] Error processing input = {:?}", err);
@@ -176,16 +78,14 @@ impl Server {
         tokio::spawn(output_writer);
     }
 
-    pub fn spawn(self, membership: Membership, dissemination: Dissemination, cache: TimeoutCache) -> thread::JoinHandle<()> {
+    pub fn spawn(self) -> thread::JoinHandle<()> {
         thread::spawn(move || {
             let bind_addr = self.addr.clone();
             let listener = TcpListener::bind(&bind_addr).unwrap();
             let server = listener
                 .incoming()
                 .for_each(move |socket| {
-                    let () =
-                        self.clone()
-                            .handle_connection(socket, membership.clone(), dissemination.clone(), cache.clone());
+                    let () = self.clone().handle_connection(socket);
                     Ok(())
                 }).map_err(|err| {
                     println!("[server] Accept error = {:?}", err);
@@ -194,4 +94,6 @@ impl Server {
             tokio::run(server)
         })
     }
+
 }
+
